eff77e3bb56cb3e00bcbb4d6f8ff3309061b09c3
Brachmann <alexandre.brachmann1@gmail.com>
2026-08-11 22:45:21 +0200

    Add login page, traitement login

---
+++ pages/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    
+<div>
+    <form method="POST" action="../traitements/traitement_login.php">
+        <div>
+            <label for="mail">Adresse mail</label>
+            <input type="email" name="mail" id="mail" placeholder="exemple@gmail.com" required>
+        </div>
+        <div>
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <button type="submit">
+            Se connecter
+        </button>
+    </form>
+</div>
+
+</body>
+</html>-
---
+++ traitements/traitement_login.php
@@ -0,0 +1,21 @@
+<?php
+
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+    if(
+        isset($_POST["mail"]) && $_POST["mail"] !== "" &&
+        isset($_POST["password"]) && $_POST["password"] !== "" )
+    {
+        $u =    ["mail"=>$_POST["mail"],
+                "password"=>$_POST["password"] ];
+            connection($u);
+    }
+    else{
+        header("Location: /teamup/pages/login.php");
+    }
+
+}
+else{
+    header("Location: /teamup/pages/login.php");
+}
