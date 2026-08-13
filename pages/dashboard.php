66d5219d40a28080567d13ff1dbbd5b2361357d5
Brachmann <alexandre.brachmann1@gmail.com>
2026-08-13 23:04:12 +0200

    Add pages dashboard.php, Add partial dashboard_user.php

---
+++ pages/dashboard.php
@@ -0,0 +1,27 @@
+<?php 
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+
+    <?php
+
+        if($_SESSION["role"] === "user"){
+            require_once "../partials/dashboard_user.php";
+        }
+        else{
+            header("Location: /teamup/pages/login.php");
+        }
+        
+
+    ?>
+
+</body>
+</html>
