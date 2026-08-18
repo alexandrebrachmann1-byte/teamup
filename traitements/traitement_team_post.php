ff3768456b96ca3bca5891279d034432e3e32879
Brachmann <alexandre.brachmann1@gmail.com>
2026-08-19 01:18:35 +0200

    Add traitement_team_post.php

--- traitements/traitement_team_post.php
+++ traitements/traitement_team_post.php
@@ -4,22 +4,20 @@ require_once "../functions/database.php";
 
 if(isset($_POST["ok"])){
 
-    $username = $_POST["username"];
+    $name = $_POST["name"];
     $rank = $_POST["rank"];
     $role = implode(',', $_POST["role"]);
-    $champion = implode(',', $_POST["champion"]);
     $description = $_POST["description"];
     $discord = $_POST["discord"];
 
 
-    $request = getpdo()->prepare("INSERT INTO player_posts (user_id, riot_username, rank, role, champion, discord, description) VALUES (:user_id, :riot_username, :rank, :role, :champion, :discord, :description)");
+    $request = getpdo()->prepare("INSERT INTO team_posts (user_id, name, rank, role, description, discord) VALUES (:user_id, :name, :rank, :role, :description, :discord)");
     $request->execute(
         array(
             "user_id" => $_SESSION["user_id"],
-            "riot_username" => $username,
+            "name" => $name,
             "rank" => $rank,
             "role" => $role,
-            "champion" => $champion,
             "description" => $description,
             "discord" => $discord
         )
