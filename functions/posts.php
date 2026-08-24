fb6ddec8af9acd7cb7007ad81597424a0e2eb30b
Brachmann <alexandre.brachmann1@gmail.com>
2026-08-24 15:31:04 +0200

    Add function get_player_post_by_user_id()

--- functions/posts.php
+++ functions/posts.php
@@ -22,3 +22,26 @@ function get_all_teams_posts() {
    
   return $teamsPosts;
 }
+
+function get_player_post_by_user_id($user_id) {
+    $pdo = getPDO();  
+    $sql = "SELECT p.id, p.user_id, p.riot_username, p.rank, p.role, p.champion, p.discord, p.description, p.created_at FROM player_posts as p INNER JOIN users AS u ON u.id = p.user_id WHERE u.id = :id";
+    $pstmt = $pdo->prepare($sql);
+    $pstmt->execute(["id" => $user_id]);
+    
+  $playerPost = $pstmt->fetchAll(PDO::FETCH_ASSOC);
+
+   
+  return $playerPost;
+}
+
+function get_team_post_by_user_id($user_id) {
+    $pdo = getPDO();  
+    $sql = "SELECT t.id, t.user_id, t.name, t.rank, t.role, t.description, t.discord, t.created_at FROM team_posts as t INNER JOIN users AS u ON u.id = t.user_id WHERE u.id = :id";
+    $pstmt = $pdo->prepare($sql);
+
+  $teamPost = $pstmt->fetchAll(PDO::FETCH_ASSOC);
+
+   
+  return $teamPost;
+}
--- partials/dashboard_user.php
+++ partials/dashboard_user.php
@@ -1,4 +1,30 @@
-<?php 
+<?php
+    require_once "../functions/posts.php";
     require_once "form_player_post.php";
     require_once "form_team_post.php";
+
+    $playerPosts = get_player_post_by_user_id($_SESSION["user_id"]);
+    
+    foreach ($playerPosts as $playerPost) {
+    ?> 
+        <div>
+            <h4> <?php echo $playerPost["riot_username"]; ?> </h4>
+        </div>
+        <div>
+            <p> <?php echo $playerPost["role"]; ?> </p>
+        </div>
+        <div>
+            <p> <?php echo $playerPost["rank"]; ?> </p>
+        </div>
+        <div>
+            <p> <?php echo $playerPost["champion"]; ?> </p>
+        </div>
+        <div>
+            <p> <?php echo $playerPost["description"]; ?> </p>
+        </div>
+        <div>
+            <p> Discord : <?php echo $playerPost["discord"]; ?> </p>
+        </div>
+    <?php
+}
 ?>
