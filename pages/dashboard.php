452ec6b400008f582a866ed83aa38103cc4d5b77
Brack <alexandre.brachmann1@gmail.com>
2026-08-28 01:57:46 +0200

    Add header.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+require_once "../partials/header.php";
 ?>
 
 <!DOCTYPE html>
---
+++ partials/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="/teamup/assets/css/style.css">
+</head>
+<body>
+   
+    <nav class="navbar">
+        <div class="navdiv">
+            <div class="logo"> <img src="../assets/images/logo_teamup.png" alt="teamup"></div>
+            <ul>
+                <li> <a href="../index.php">Acceuil</a> </li>
+                <li> <a href="../pages/dashboard.php">Dashboard</a> </li>
+                <li> <a href="../pages/player_posts.php">Chercher un joueur</a> </li>
+                <li> <a href="../pages/team_posts.php">Chercher une équipe</a> </li>
+                <button> <a href="../pages/login.php">Se connecter</a> </button>
+                <button> <a href="../pages/register.php">S'inscrire</a> </button>
+            </ul>
+        </div>
+    </nav>
+
+</body>
+</html>
