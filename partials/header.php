6f1283a38a15e16b3f7e53bcd3c679e29fbdf455
Brack <alexandre.brachmann1@gmail.com>
2026-08-29 02:32:16 +0200

    Update header.php -> If connected : button logout, if not connected : button login/register

--- partials/header.php
+++ partials/header.php
@@ -16,11 +16,26 @@
                 <li> <a href="../pages/dashboard.php">Dashboard</a> </li>
                 <li> <a href="../pages/player_posts.php">Chercher un joueur</a> </li>
                 <li> <a href="../pages/team_posts.php">Chercher une équipe</a> </li>
-                <button> <a href="../pages/login.php">Se connecter</a> </button>
-                <button> <a href="../pages/register.php">S'inscrire</a> </button>
+
+                <?php if (isset($_SESSION["role"])) { ?>  
+                        <button> <a href="../pages/logout.php">Se deconnecter</a> </button>
+                <?php }else{ ?>
+                        
+                        <button> <a href="../pages/login.php">Se connecter</a> </button>
+                        <button> <a href="../pages/register.php">S'inscrire</a> </button>
+                <?php } ?>
             </ul>
         </div>
     </nav>
 
 </body>
 </html>
+
+
+<?php
+
+if (isset($_SESSION["role"])) {
+ 
+
+}
+var_dump($_SESSION);
