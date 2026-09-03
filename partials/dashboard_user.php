d1b3c508234befb84bf5bfd23c78e6ae23594993
Brack <alexandre.brachmann1@gmail.com>
2026-09-03 11:52:25 +0200

    Add edit and delete link on players posts

--- partials/dashboard_user.php
+++ partials/dashboard_user.php
@@ -8,29 +8,38 @@
 
 <div class="posts-grid">
     <?php foreach ($playerPosts as $playerPost) { ?>
-                <a href="../pages/player_post_details.php?id=<?php echo $playerPost["id"]; ?>" class="post-card">
-                    <h4 class="post-card-title"><?php echo $playerPost["riot_username"]; ?></h4>
+        <div class="post-card">
+            <a href="../pages/player_post_details.php?id=<?php echo $playerPost["id"]; ?>" class="post-card-link">
+                <h4 class="post-card-title"><?php echo $playerPost["riot_username"]; ?></h4>
 
-                    <div class="post-card-row">
-                        <span class="post-card-label">Rôle</span>
-                        <span class="post-card-value"><?php echo $playerPost["role"]; ?></span>
-                    </div>
-                    <div class="post-card-row">
-                        <span class="post-card-label">Rang</span>
-                        <span class="post-card-value post-card-rank"><?php echo $playerPost["rank"]; ?></span>
-                    </div>
-                    <div class="post-card-row">
-                        <span class="post-card-label">Champions</span>
-                        <span class="post-card-value"><?php echo $playerPost["champion"]; ?></span>
-                    </div>
+                <div class="post-card-row">
+                    <span class="post-card-label">Rôle</span>
+                    <span class="post-card-value"><?php echo $playerPost["role"]; ?></span>
+                </div>
+                <div class="post-card-row">
+                    <span class="post-card-label">Rang</span>
+                    <span class="post-card-value post-card-rank"><?php echo $playerPost["rank"]; ?></span>
+                </div>
+                <div class="post-card-row">
+                    <span class="post-card-label">Champions</span>
+                    <span class="post-card-value"><?php echo $playerPost["champion"]; ?></span>
+                </div>
 
-                    <p class="post-card-description"><?php echo $playerPost["description"]; ?></p>
+                <p class="post-card-description"><?php echo $playerPost["description"]; ?></p>
 
-                    <div class="post-card-footer">
-                        Discord : <span class="post-card-discord"><?php echo $playerPost["discord"]; ?></span>
-                    </div>
-                </a>
+                <div class="post-card-footer">
+                    Discord : <span class="post-card-discord"><?php echo $playerPost["discord"]; ?></span>
+                </div>
+            </a>
+
+            <?php if ($playerPost["user_id"] === $_SESSION["user_id"]) { ?>
+                <div class="post-card-actions">
+                    <a href="../pages/player_post_edit.php?id=<?php echo $playerPost["id"]; ?>" class="btn-card btn-card-edit">Modifier</a>
+                    <a href="../traitements/player_post_delete.php?id=<?php echo $playerPost["id"]; ?>" class="btn-card btn-card-delete">Supprimer</a>
+                </div>
             <?php } ?>
+        </div>
+    <?php } ?>
 </div>
 
 <?php
