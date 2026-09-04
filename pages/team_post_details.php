fd37e3cbd092fdc7e2554e6cc93bb58f0dbfd354
Brack <alexandre.brachmann1@gmail.com>
2026-09-04 13:40:27 +0200

    Add team_post_details.php

---
+++ pages/team_post_details.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+require_once "../functions/posts.php"
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Détails d'annonce d'équipe</title>
+    <link rel="stylesheet" href="/teamup/assets/css/style.css">
+</head>
+<body>
+    <?php require_once "../partials/header.php"; ?>
+        <div class="page-content">
+                <?php 
+                $teamPost = get_team_post_by_id($_GET["id"]);
+                $teamPost = $teamPost[0];
+                ?>
+            <div class="posts-grid">
+                <div class="post-card">
+                    <h4 class="post-card-title"><?php echo $teamPost["name"]; ?></h4>
+
+                        <div class="post-card-row">
+                            <span class="post-card-label">Rang</span>
+                            <span class="post-card-value post-card-rank"><?php echo $teamPost["rank"]; ?></span>
+                        </div>
+                        <div class="post-card-row">
+                            <span class="post-card-label">Rôle(s) recherché(s)</span>
+                            <span class="post-card-value"><?php echo $teamPost["role"]; ?></span>
+                        </div>
+
+                        <p class="post-card-description"><?php echo $teamPost["description"]; ?></p>
+
+                        <div class="post-card-footer">
+                            Discord : <span class="post-card-discord"><?php echo $teamPost["discord"]; ?></span>
+                        </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+</body>
+</html>
